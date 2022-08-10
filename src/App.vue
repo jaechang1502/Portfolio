fe86f0909663d5adace295d57c20dc9142c5f4ca
jaechang1502 <jaechang92@naver.com>
2022-08-10 15:53:52 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  gds
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './plugin/css.js'
+import App from './App.vue'
+import router from './router/router'
+import store from './store/index'
+
+createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
