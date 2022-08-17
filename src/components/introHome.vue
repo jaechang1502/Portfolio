4ac3549e10b8d46d073e140020281e3e2cc6d886
jaechang1502 <jaechang92@naver.com>
2022-08-17 14:25:36 +0900

    타이핑

--- src/components/introHome.vue
+++ src/components/introHome.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>ㅎㅇ</div>
+    <div class="Home">
+        <div class="fw800">
+            <vue-typer text='watermelon'></vue-typer>   
+        </div>
+        
+    </div>
 </template>
 
 <script>
+import {VueTyper} from 'vue-typer'
 export default {
+    components:{
+        VueTyper
+    }
+    
+   
 
 }
 </script>
 
 <style>
-
+    
 </style>
