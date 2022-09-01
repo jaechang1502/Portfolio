2a1d83c3c30a3093f6b41baa1d01ebea2730a40e
jaechang1502 <jaechang92@naver.com>
2022-09-01 17:09:08 +0900

    모바일 완성

--- src/components/CareerComponent.vue
+++ src/components/CareerComponent.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <div class="absolute font-extrabold text-5xl career ">Career</div>
+    <div class="absolute font-extrabold sm:text-5xl xxs:text-2xl career ">Career</div>
   <div>
-    <div class="text-3xl p-6 mb-4 drop-shadow-xl bg-white-trasperate rounded-lg">
+    <div class="sm:text-3xl xxs:text-xl sm:p-6 xxs:p-3 sm:mb-4 xxs:mb-5 drop-shadow-xl bg-white-trasperate rounded-lg">
         <div class="flex">
             <div class="font-extrabold">(주)스튜디오질풍</div>
             <div class="mx-2">|</div>
@@ -11,7 +11,7 @@
         <div class="text-base mt-2 text-slate-500">2021년 10월 - 2022년 8월 | 10개월 근무</div>
         <div class="text-base mt-2">웹툰 모델 판매사이트와 이미지에디터사이트를  기획, 서버, 웹사이트, 입점자관리서비스를 개발 했습니다.</div>
     </div>
-    <div class="text-3xl p-6 drop-shadow-xl bg-white-trasperate rounded-lg">
+    <div class="sm:text-3xl xxs:text-xl sm:p-6 xxs:p-3 sm:mb-4 xxs:mb-2 drop-shadow-xl bg-white-trasperate rounded-lg">
         <div class="flex">
             <div class="font-extrabold">KIMCHIMAN Develop</div>
             <div class="mx-2">|</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xxs': '330px',
+      'xs': '475px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    extend: {
+      screens: {
+        'mobile': {'max': '475'},
+      },
+    },
   },
   plugins: [],
 }
